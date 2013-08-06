788c5b445d07879e9567c81d416bde75590ac3e7
Gabriel Horner <gabriel.horner@gmail.com>
2013-08-05 21:49:21 -0400

    pass a map through all wrap-fns
    also removed old wrap-transform

--- src/io/pedestal/app/dataflow_logger/core.clj
+++ src/io/pedestal/app/dataflow_logger/core.clj
@@ -8,7 +8,7 @@
 (def log-args (partial log-string "received arguments"))
 (def log-return (partial log-string "returned"))
 
-(defn wrap-fn [f log-fn log-string-args]
+(defn wrap-fn [f {:keys [log-fn]} log-string-args]
   (let [msg (apply log-return log-string-args)]
     (fn [& args]
       #_(log-fn msg args)
@@ -16,37 +16,34 @@
         (log-fn msg ret)
         ret))))
 
-(defn- wrap-transform2 [log-fn]
+(defn- wrap-transform [options]
   (fn [[op path f]]
-    [op path (log-fn (log-string "TRANSFORM" op path) f)]))
+    [op path (wrap-fn f options ["TRANSFORM" op path])]))
 
-(defn- wrap-transform [log-fn]
-  (fn [[op path f]]
-    [op path (wrap-fn f log-fn ["TRANSFORM" op path])]))
-
-(defn- wrap-derive [log-fn]
+(defn- wrap-derive [options]
   (fn [[input output f & args]]
-    (vec (concat [input output (wrap-fn f log-fn ["DERIVE" input output])]
+    (vec (concat [input output (wrap-fn f options ["DERIVE" input output])]
                  args))))
 
-(defn- wrap-effect [log-fn]
+(defn- wrap-effect [options]
   (fn [[paths f & args]]
-    (vec (concat [paths (wrap-fn f log-fn ["EFFECT" paths])]
+    (vec (concat [paths (wrap-fn f options ["EFFECT" paths])]
                  args))))
 
-(defn- wrap-emit [log-fn]
+(defn- wrap-emit [options]
   (fn [emit-entry]
     (if (map? emit-entry)
-      {:init (wrap-fn (:init emit-entry) log-fn ["EMIT" :init])}
+      {:init (wrap-fn (:init emit-entry) options ["EMIT" :init])}
       (if (vector? emit-entry)
         (let [[paths f] emit-entry]
-          [paths (wrap-fn f log-fn ["EMIT" paths])])
+          [paths (wrap-fn f options ["EMIT" paths])])
         ;; TODO: Remove once emit has been reviewed. Yes this fails in cljs
         (prn "EMIT not handled" emit-entry)))))
 
 (defn wrap-app [app log-fn]
-  (assoc app
-         :transform (mapv (wrap-transform log-fn) (:transform app))
-         :derive (set (map (wrap-derive log-fn) (:derive app)))
-         :effect (set (map (wrap-effect log-fn) (:effect app)))
-         :emit (mapv (wrap-emit log-fn) (:emit app))))
+  (let [options {:log-fn log-fn}]
+    (assoc app
+           :transform (mapv (wrap-transform options) (:transform app))
+           :derive (set (map (wrap-derive options) (:derive app)))
+           :effect (set (map (wrap-effect options) (:effect app)))
+           :emit (mapv (wrap-emit options) (:emit app)))))
